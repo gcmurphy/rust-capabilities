6cb88a3fab296050ffc6a4a83bacbe1b9412e472
Grant Murphy <grantcmurphy@gmail.com>
2016-01-20 14:52:21 -0800

    Update documentation

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['capabilities'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='capabilities/struct.Capability.html' title='capabilities::Capability'>Capability</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='capabilities/struct.Capabilities.html' title='capabilities::Capabilities'>Capabilities</a>",];implementors['capabilities'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='capabilities/struct.Capability.html' title='capabilities::Capability'>Capability</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='capabilities/struct.Capabilities.html' title='capabilities::Capabilities'>Capabilities</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
